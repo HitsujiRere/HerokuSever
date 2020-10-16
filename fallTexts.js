e938ef835aed35641b5f3a749db4a12047326e31
HitsujiRere <hitsujirere@gmail.com>
2020-10-16 17:16:23 +0900

    Add Movement

--- fallTexts.js
+++ fallTexts.js
@@ -5,8 +5,19 @@ const { socketio } = require('./app');
 const express = require('express')
 const router = express.Router()
 
+socketio.on('connection', (socket) => {
+    socket.on('fallTexts_message', (msg) => {
+        console.log('message: ' + msg);
+        socketio.emit('fallTexts_message', msg);
+    });
+});
+
 router.use('/', async (req, res) => {
     res.render('fallTexts.ejs');
 });
+
+setInterval(() => {
+    socketio.emit('fallTexts_message', new Date().toLocaleTimeString());
+}, 1000);
 
 module.exports = router;
